eadbbaf89aff2f79f05dde25fc2d1a48bb983449
Nikita Nazarov <nvnazarov.me@gmail.com>
2026-08-18 22:57:54 +0300

    docs: add docs for the package and the function

--- add.go
+++ add.go
@@ -1,5 +1,11 @@
+// Package sum provides functionality for adding
+// two integer numbers.
 package sum
 
+// Add adds two integers together.
+// See [addition].
+//
+// [addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
