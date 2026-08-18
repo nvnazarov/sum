81591526064009080ba3ecb9e334d391c4864985
Nikita Nazarov <nvnazarov.me@gmail.com>
2026-08-18 23:09:33 +0300

    feat: generalize the Add function

--- add.go
+++ add.go
@@ -1,5 +1,17 @@
+// Package sum provides functionality for adding
+// two numbesr together.
 package sum
 
-func Add(a, b int) int {
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add adds two numbers (integers or floats) together.
+// See [addition].
+//
+// [addition]: https://www.mathsisfun.com/numbers/addition.html
+func Add[T Number](a, b T) T {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nvnazarov/sum
 
 go 1.25.1
+
+require golang.org/x/exp v0.0.0-20260813180055-c1d0aacb2297
